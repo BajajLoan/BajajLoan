44ad3ff7a4e0369b50cdb08221a92c239652a1aa
BajajLoan <serviceinvestor.bajaj@gmail.com>
2026-01-30 04:22:38 +0530

    one Signal removed

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,6 @@ import "./index.css"
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-// import { initOneSignal } from "../src/oneSignal.js";
-
-// initOneSignal();
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
