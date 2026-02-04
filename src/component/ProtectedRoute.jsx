96691555cd3927d0f1008792bd7d3e84015ba91e
BajajLoan <serviceinvestor.bajaj@gmail.com>
2026-02-04 15:37:11 +0530

    njkkl

--- src/component/ProtectedRoute.jsx
+++ src/component/ProtectedRoute.jsx
@@ -1,35 +1,13 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../navigation/AuthContext";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const ProtectedRoute = ({ children }) => {
-  const [token,setIsToken]=useState(null)
-
-  const isTokenExpired = (token) => {
-  try {
-    const payload = JSON.parse(atob(token.split(".")[1]));
-    return payload.exp * 1000 < Date.now();
-  } catch {
-    return true;
-  }
-};
-const logout = () => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("email");
-    setIsToken(null);
-  };
- useEffect(() => {
-    const storedToken = localStorage.getItem("token");
-
-    if (!storedToken) return;
-
-    if (isTokenExpired(storedToken)) {
-      logout();
-    } else {
-      setIsToken(storedToken);
-      // setAutoLogout(storedToken);
-    }
-  }, []);
+  const [token,setToken]=useState(null)
+  useEffect(()=>{
+    const res=localStorage.getItem("token")
+    setToken(res)
+  },[])
 
   if (!token) {
     return <Navigate to="/login" replace={"/"} />;
